1cb4916e67b7cb8784f6d9566c06483128556b70
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-24 05:37:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pyxlsb import open_workbook as open_xlsb
 def install_npm_and_tweet_harvest():
     # Perintah instalasi (contoh untuk Ubuntu/Debian)
     commands = [
-        "sudo apt install npm",
+        #"sudo apt install npm",
         "npm install -g tweet-harvest"
     ]
 
